88b815dee6d6823abd39db3307d0f0fc71096825
nickydz12 <115716109+nickydz12@users.noreply.github.com>
2024-07-21 14:14:23 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -36,7 +36,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Septahadif/instalasi/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/nickydz12/kentod/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin

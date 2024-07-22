92eddeec1990e6e9121d0d39bd6dc507681d2af8
nickydz12 <115716109+nickydz12@users.noreply.github.com>
2024-07-22 12:03:53 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -540,7 +540,7 @@ print_success "Password SSH"
 function udp_mini(){
 clear
 print_install "Memasang Service Limit IP & Quota"
-wget -q https://raw.githubusercontent.com/Septahadif/instalasi/main/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
+wget -q https://raw.githubusercontent.com/nickydz12/kentod/main/config/fv-tunnel && chmod +x fv-tunnel && ./fv-tunnel
 
 # // Installing UDP Mini
 mkdir -p /usr/local/kyt/
